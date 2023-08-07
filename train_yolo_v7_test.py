a0fc32469818fb38beb32247434fccd806497eae
ambroise <ambroise.berthe@ikomia.com>
2023-08-07 17:38:21 +0200

    Fix test file

--- train_yolo_v7_test.py
+++ train_yolo_v7_test.py
@@ -1,5 +1,4 @@
 import logging
-from ikomia.core import task, ParamMap
 from ikomia.utils.tests import run_for_test
 import os
 
@@ -9,10 +8,10 @@ logger = logging.getLogger(__name__)
 def test(t, data_dict):
     logger.info("===== Test::train yolo v7 =====")
     input_dataset = t.get_input(0)
-    params = task.get_parameters(t)
-    params["epochs"] = 2
-    params["batch_size"] = 1
-    params["dataset_split_ratio"] = 0.5
-    task.set_parameters(t, params)
+    params = t.get_parameters()
+    params["epochs"] = "2"
+    params["batch_size"] = "1"
+    params["dataset_split_ratio"] = "0.5"
+    t.set_parameters(params)
     input_dataset.load(data_dict["datasets"]["detection"]["dataset_coco"])
     yield run_for_test(t)
